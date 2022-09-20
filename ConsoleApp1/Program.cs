764b3bc6288fb3f2810104bb4ff903e34eec0777
Raghu Paruchuri <raghutecra@hotmail.com>
2022-09-20 09:48:29 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+DateTime EndDate = DateTime.Now;
+EndDate = EndDate.AddDays(-10);
+if (EndDate != new DateTime() && !string.IsNullOrEmpty(EndDate.ToString()))
+{
+    Console.WriteLine("EndDate is less than current date");
+}
+EndDate = EndDate.AddDays(10);
+if (EndDate != new DateTime() && !string.IsNullOrEmpty(EndDate.ToString()))
+{
+    Console.WriteLine("EndDate is greater than current date");
+}
